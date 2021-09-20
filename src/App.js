8c3709d3bd9a9531205b41b385c26fbfa997a4d0
enhorme <enhorwp@gmail.com>
2021-09-20 20:28:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css'
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particleOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
 }
 
-export default App;
+export class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+             <Particles className='particle'
+                params={particleOptions} />
+        
+        <Navigation />
+         <Logo />
+         <Rank />
+            <ImageLinkForm />
+             {/* <FaceRecognition/> */}
+      </div>
+    )
+  }
+}
+
+export default App
+
